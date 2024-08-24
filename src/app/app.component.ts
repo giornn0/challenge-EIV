f356b1babafe96544e46c0b878510f19601f8abe
giornn0 <rusty.arrebol@proton.me>
2024-08-24 04:28:35 -0300

    feat: main app setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent, FooterComponent, ThemeService } from '@core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  providers: [ThemeService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'challenge-EIV';
+  themeSelected = inject(ThemeService).$selectedTheme;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent, NotFoundComponent } from '@core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'vendedores',
+    loadChildren: () => import('./features/sellers/sellers.routes'),
+  },
+  {
+    path: 'localidades',
+    loadChildren: () => import('./features/locations/locations.routes'),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
