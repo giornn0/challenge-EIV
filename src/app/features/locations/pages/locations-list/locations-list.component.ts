d9aab0245bd48a6c162c0b5920437e14c7de84af
giornn0 <rusty.arrebol@proton.me>
2024-08-26 14:34:30 -0300

    feat: locations list

--- src/app/features/locations/pages/locations-list/locations-list.component.ts
+++ src/app/features/locations/pages/locations-list/locations-list.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Location } from '@app-features/locations/location.model';
+import { LocationsApiService } from '@app-features/locations/services';
+import { ContainerComponent, KeyOf, RecordSome } from '@shared';
 
 @Component({
   selector: 'app-locations-list',
   standalone: true,
-  imports: [],
+  imports: [ContainerComponent, AsyncPipe],
   templateUrl: './locations-list.component.html',
-  styleUrl: './locations-list.component.scss'
+  styleUrl: './locations-list.component.scss',
 })
 export class LocationsListComponent {
+  tableInfo: RecordSome<Location, string> = {
+    id: 'Id',
+    localidad: 'Localidad',
+    codigoPostal: 'Código postal',
+  };
+  headers: string[];
+  columnAccesors: KeyOf<Location>[];
 
+  service = inject(LocationsApiService);
+  locationsList = this.service.getAll();
+
+  constructor() {
+    this.headers = Object.values(this.tableInfo);
+    this.columnAccesors = <KeyOf<Location>[]>Object.keys(this.tableInfo);
+  }
 }
